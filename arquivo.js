874de5f7e7df2ad9029598ac9063d271e7d6a72f
JamileMacedo <millycentenario@gmail.com>
2024-03-07 21:42:36 -0300

    Atualizações e exclusão de imagens

--- arquivo.js
+++ arquivo.js
@@ -4,8 +4,24 @@ const convertButton = document.querySelector(".button-convert")
 function convertValues(){
 
     const inputMoedaValue = document.querySelector(".input-moeda").value
+    
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConvert = document.querySelector(".currency-value")
+
+
     const dolarToday = 5.2
     const convertedValue = inputMoedaValue / dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputMoedaValue)
+    currencyValueConvert.innerHTML = convertedValue
+
+    currencyValueConvert.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
 
     console.log(convertedValue)
 
